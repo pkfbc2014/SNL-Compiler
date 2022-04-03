1dc754e3a066ec2dafd3d3bf5458e67b913d76f0
PanDapao <1476822714@qq.com>
2022-04-03 11:55:18 +0800

    pkf

--- SNL/main.cpp
+++ SNL/main.cpp
@@ -2,23 +2,41 @@
 
 #include "head.h"
 
+char Non_symbol[67][20]; // 67个非终结符
+char Reserved_word[42][20]; // 42个保留字和其他符号
+
 int main()
 {
 	FILE* fp;
     char input_filename[20] = {"test.txt"}; // 输入文件为test.txt
 
-    if ((fp = fopen(input_filename, "r")) == NULL) // 读入非终结符文件
+    /*===================================================================*/
+    if ((fp = fopen("Non_symbol.txt", "r")) == NULL) // 读入非终结符文件
     {
         printf("cannot open the Non_symbol file!\n"); // 文件打开失败
         exit(0); // 结束进程
     }
+    int i = 0;
+    while (fgets(Non_symbol[i], 20, fp) != NULL)
+    {
+        i++;
+    }
+    fclose(fp);
 
-    if ((fp = fopen(input_filename, "r")) == NULL) // 读入保留字文件
+    /*===================================================================*/
+    if ((fp = fopen("Reserved_word.txt", "r")) == NULL) // 读入保留字文件
     {
         printf("cannot open the Reserved_word file!\n"); // 文件打开失败
         exit(0); // 结束进程
     }
+    int i = 0;
+    while (fgets(Reserved_word[i], 20, fp) != NULL)
+    {
+        i++;
+    }
+    fclose(fp);
 
+    /*===================================================================*/
     if ((fp = fopen(input_filename, "r")) == NULL) // 读入test文件
     {
         printf("cannot open the file!\n"); // 文件打开失败
