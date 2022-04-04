a9f47f4af91d5a9fb31bfe18d3eb7a2402caab17
PanDapao <1476822714@qq.com>
2022-04-04 14:18:33 +0800

    pkf

--- SNL/func_statement.h
+++ SNL/func_statement.h
@@ -2,10 +2,10 @@
 
 //一些函数的声明
 
+int getNonIndex(char* s); // 返回非终结符s在非终结符集合中的下标
 void cal_first(); // 计算first集
 void cal_follow(); // 计算follow集
-void output_fitst(); //输出first集到本地
-void output_follow(); //输出follow集到本地
+void out_fitstfollow(); //输出first集和follow集到本地
 
 void LL1_analysis(); //LL1分析法
 void RD_analysis(); //递归下降分析法
