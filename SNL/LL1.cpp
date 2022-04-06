3083104b803d67a6f9ec87cf0082002800852d57
PanDapao <1476822714@qq.com>
2022-04-06 18:33:42 +0800

    pkf

--- SNL/LL1.cpp
+++ SNL/LL1.cpp
@@ -6,8 +6,7 @@
 #include "func_statement.h"
 #include "global_var.h"
 
-
-void LL1_analysis()
+void LL1_analysis() // LL1分析法
 {
 	cal_predict(); // 计算predict集并构造LL1预测分析表
 	out_predict(); // 输出LL1预测分析表到本地
--- SNL/RD.cpp
+++ SNL/RD.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void RD_analysis()
+void RD_analysis() // µİ¹éÏÂ½µ·¨
 {
 
 }
