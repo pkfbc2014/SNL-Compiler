565a0a4e85f422646c3e19bb8b3d30b762413f12
PanDapao <1476822714@qq.com>
2022-04-07 19:09:35 +0800

    pkf

--- Resource/RD.cpp
+++ Resource/RD.cpp
@@ -3,8 +3,242 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "RD_head.h"
 
 void RD_analysis() // µÝ¹éÏÂ½µ·¨
+{
+
+}
+
+treenode* program()
+{
+
+}
+treenode* programHead()
+{
+
+}
+treenode* declarePart()
+{
+
+}
+treenode* programBody()
+{
+
+}
+treenode* typeDecPart()
+{
+
+}
+treenode* varDecPart()
+{
+
+}
+treenode* procDecpart()
+{
+
+}
+treenode* typeDec()
+{
+
+}
+treenode* typeDecList()
+{
+
+}
+treenode* typeID()
+{
+
+}
+treenode* typeDef()
+{
+
+}
+treenode* typeDecMore()
+{
+
+}
+treenode* baseType()
+{
+
+}
+treenode* structureType()
+{
+
+}
+treenode* arrayType()
+{
+
+}
+treenode* recType()
+{
+
+}
+treenode* fieldDecList()
+{
+
+}
+treenode* IDList()
+{
+
+}
+treenode* fieldDecMore()
+{
+
+}
+treenode* IDMore()
+{
+
+}
+treenode* varDec()
+{
+
+}
+treenode* varDecList()
+{
+
+}
+treenode* varIDList()
+{
+
+}
+treenode* varDecMore()
+{
+
+}
+treenode* varIDMore()
+{
+
+}
+treenode* procDec()
+{
+
+}
+treenode* paramList()
+{
+
+}
+treenode* procDecPart()
+{
+
+}
+treenode* procBody()
+{
+
+}
+treenode* paramDecList()
+{
+
+}
+treenode* param()
+{
+
+}
+treenode* paramMore()
+{
+
+}
+treenode* formList()
+{
+
+}
+treenode* fidMore()
+{
+
+}
+treenode* stmList()
+{
+
+}
+treenode* stm()
+{
+
+}
+treenode* stmMore()
+{
+
+}
+treenode* conditionalStm()
+{
+
+}
+treenode* loopStm()
+{
+
+}
+treenode* inputStm()
+{
+
+}
+treenode* outputStm()
+{
+
+}
+treenode* returnStm()
+{
+
+}
+treenode* assCall()
+{
+
+}
+treenode* assignmentRest()
+{
+
+}
+treenode* callStmRest()
+{
+
+}
+treenode* variMore()
+{
+
+}
+treenode* exp()
+{
+
+}
+treenode* actparamList()
+{
+
+}
+treenode* actparamMore()
+{
+
+}
+treenode* term()
+{
+
+}
+treenode* otherTerm()
+{
+
+}
+treenode* addOp()
+{
+
+}
+treenode* factor()
+{
+
+}
+treenode* otherFactor()
+{
+
+}
+treenode* multOp()
+{
+
+}
+treenode* variable()
+{
+
+}
+treenode* fieldVar()
+{
+
+}
+treenode* fieldVarMore()
 {
 
 }
