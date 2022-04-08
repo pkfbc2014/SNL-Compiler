b6665ccd129c2fedfcb14eba2c19a783b6b9b41e
1127960155 <1127960155@qq.com>
2022-04-08 19:02:22 +0800

    完善词法分析

--- Resource/LexicalAnalysis.h
+++ Resource/LexicalAnalysis.h
@@ -5,15 +5,42 @@
 #include <stdlib.h>
 //类型声明
 enum state { START = 1, INID, INNUM, DONE, INASSIGN, INCOMMENT, INRANGE, INCHAR};//DFA状态
-enum error{NORMAL=1,INASSIGN_ERROR,INRANGE_ERROR,INCHAR_ERROR,ERROR};//错误类型分别对应无错误,赋值错误，程序结束错误,字符错误，读入无法识别错误
-enum LexType{ID=1,RESERVED,INTC,SINGLE,DOUBLE,COM_LEFT,COM_RIGHT,CHAR,SUBSCRIPT};//单词分类的枚举类型：标识符，保留字，无符号整数，单字符分界符，双字符分界符,注释头符，注释结束符，字符起始符和结束符，数组下标界限符
-struct node//链表结点定义
+enum error{NORMAL=1,INASSIGN_ERROR,INRANGE_ERROR,INCHAR_ERROR,ERROR1};//错误类型分别对应无错误,赋值错误，程序结束错误,字符错误，读入无法识别错误
+typedef enum//词法类型定义
+{	//簿记单词符
+	ENDFILE, ERROR,
+	//保留字
+	PROGRAM, PROCEDURE, TYPE, VAR, IF,
+	THEN, ELSE, FI, WHILE, DO, ENDWH,
+	BEGIN, END1, READ, WRITE, ARRAY, OF,
+	RECORD, RETURN1,	
+	//类型
+	INTEGER, CHAR1,
+	//多字符单词符号
+	ID, INTC, CHARC,
+	//特殊符号
+	ASSIGN, EQ, LT, PLUS, MINUS,
+	TIMES, OVER, LPAREN, RPAREN, DOT,
+	COLON, SEMI, COMMA, LMIDPAREN, RMIDPAREN,
+	UNDERANGE//数组下标
+}LexType;
+
+static struct word {//保留字表，便于查阅
+	char Sem[100];   //语义信息
+	LexType tok; //词法信息
+}reservedWords[21] = { {"program",PROGRAM},{"type",TYPE},{"var",VAR},
+	{"procedure",PROCEDURE},{"begin",BEGIN},{"end",END1},{"array",ARRAY},
+	{"of",OF},{"record",RECORD},{"if",IF},{"then",THEN},{"else",ELSE},{"fi",FI},
+	{"while",WHILE},{"do",DO},{"endwh",ENDWH},{"read",READ},{"write",WRITE},
+	{"return",RETURN1},{"integer",INTEGER},{"char",CHAR1} };	//保留字
+
+struct token//链表结点定义
 {
 	int Lineshow;//单词所在行数
 	LexType Lex;
 	char Sem[100]={'\0'};//单词语义信息
-	node* pre;
-	node* next;
+	token* pre;
+	token* next;
 };
 
 
@@ -26,8 +53,9 @@ char ch;//取字符后放这里
 //函数声明
 char getNextChar();//从源文件中读取一个字符
 int ungetNextChar();//将文件现有指针向前移动一个字符大小   ....!!!!应增强鲁棒性
-int init_node(node* ptr);
-int classify(char ch);
-node* getTokenList();
+int init_node(token* ptr);
+int classify(char ch);//自动机所需的对输入字符的分类函数
+LexType classify1(char ch);//单分界符的分类函数
+token* getTokenList();
 int reservedLookup();
 int ChainToFile();
--- Resource/test_LexicalAnalysis.cpp
+++ Resource/test_LexicalAnalysis.cpp
@@ -1,10 +1,13 @@
 //本文件仅用于测试词法分析功能
 //push之前应将LL1,func,main加入项目
+
+//1.注意LEXTYPE是否全部用上 2.正常生成行数信息
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include"LexicalAnalysis.h"
+#include "global_var.h"//使用保留字表
 char getNextChar() {//取得下个非 空 字 符
     char ch = fgetc(fp);
     while (ch == ' ' || ch == '\n'||ch=='\t') {
@@ -16,12 +19,12 @@ int ungetNextChar() {
     fseek(fp, -(long)sizeof(char), SEEK_CUR);
     return 0;
 }
-int classify(char ch) {
+int classify(char ch) {//无空白
     if ((ch > '@' && ch < '[') || (ch > '`' && ch < '{'))
         return 1;//字母
     if (ch >= '0' && ch <= '9')
         return 2;//数字
-    if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '(' || ch == ')' || ch == ';' || ch == '[' || ch == ']' || ch == '=' || ch == '<' || ch == EOF || ch == ' ')
+    if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '(' || ch == ')' || ch == ';' || ch == '[' || ch == ']' || ch == '=' || ch == '<' || ch == EOF)
         return 3;//单分界符
     if (ch == ':')
         return 4;//双分界符，接收为：
@@ -33,104 +36,209 @@ int classify(char ch) {
         return 7;//字符状态
     return 8;
 }
-int init_node(node* ptr) {//链表结点初始化
+LexType classify1(char ch) {
+    if (ch == '+') {
+        return PLUS;
+    }
+    else if (ch == '-') {
+        return MINUS;
+    }
+    else if (ch == '*') {
+        return TIMES;
+    }
+    else if (ch == '/') {
+        return OVER;
+    }
+    else if (ch == '(') {
+        return LPAREN;
+    }
+    else if (ch == ')') {
+        return RPAREN;
+    }
+    else if (ch == ';') {
+        return SEMI;
+    }
+    else if (ch == '[') {
+        return LMIDPAREN;
+    }
+    else if (ch == ']') {
+        return RMIDPAREN;
+    }
+    else if (ch == '=') {
+        return EQ;
+    }
+    else if (ch == '<') {
+        return LT;
+    }
+    else if (ch == EOF) {
+        return ENDFILE;
+    }
+
+}
+int init_node(token* ptr) {//链表结点初始化
     ptr->Lex = ID;//初始化为标识符
     ptr->next = ptr->pre = nullptr;
     ptr->Lineshow = -1;
     return 0;
 }
-//node* getTokenList() {//未完成：在状态转移过程中的token同时生成;有错误的处理方法
-//    if (fp == nullptr) {
-//        printf("文件打开失败！\n");
-//        return nullptr;
-//    }
-//    //链表结点处理
-//    node* head = (node*)malloc(sizeof(node)),*current=head,*next= (node*)malloc(sizeof(node));
-//    init_node(head); init_node(next);
-//    current->next = next; next->pre = current;
-//
-//    state state0 = START;
-//    char ch = getNextChar();
-//
-//    //DFA字符接收器,每次结束后应恢复
-//    char receiver[100]=" ";
-//    int num = 0;
-//
-//    //未添加读新字符的循环
-//    //添加出口状态?(可以添加标识符，当标识过程未完成时，持续循环switch语句
-//    //
-//
-//    switch (state0) {//处理逻辑：先看状态，再看输入
-//    case START:
-//        switch (classify(ch)) {
-//        case 1:
-//            state0 = INID; receiver[num++] = ch; break;
-//        case 2:
-//            state0 = INNUM; receiver[num++] = ch; break;
-//        case 3:
-//            state0 = DONE; receiver[num++] = ch; break;
-//        case 4:
-//            receiver[num++] = ch;
-//            ch = getNextChar();
-//            if (ch == '=') {
-//                state0 = INASSIGN; 
-//                receiver[num++] = ch;
-//                break;
-//            }
-//            else {
-//                state0 = START; error0 = INASSIGN_ERROR; break;//此处错误为赋值":="中':'后所跟并非'='
-//            }
-//        case 5:state0 = INCOMMENT;receiver[num++] = ch; break;//注释状态
-//        case 6:
-//            receiver[num++] = ch;
-//            ch = getNextChar();
-//            if (ch == '.') {
-//                state0 = INRANGE; receiver[num++] = ch; break;//数组下标界限状态
-//            }
-//            else {
-//                state0 = START; error0 = INRANGE_ERROR; break;//此处错误为赋值":="中':'后所跟并非'='
-//            }
-//        case 7://无需录入
-//            state0 = INCHAR; break;
-//        default://输入无法识别
-//            error0 = ERROR; receiver[num++] = ch; break;
-//        }
-//        break;
-//    case INID:
-//        ch = getNextChar();
-//        while (classify(ch) == 1 || classify(ch) == 2)
-//        {
-//            receiver[num++] = ch;
-//            ch = getNextChar();
-//        }
-//        receiver[num] = '\0';
-//        printf("识别标识符：%s\n", receiver);
-//        //标识符识别，Token建立
-//        ungetNextChar();
-//        break;
-//    case INNUM:
-//        ch = getNextChar();
-//        while (classify(ch) == 2) {
-//            receiver[num++] = ch;
-//            ch = getNextChar();
-//        }
-//        receiver[num] = '\0';
-//        printf("识别数字：%s\n", receiver);
-//        //标识符识别，Token建立
-//        ungetNextChar();
-//        break;
-//    case DONE:
-//        if (classify(ch) == 1 || classify(ch) == 2) {
-//            ch = getNextChar();
-//            if (ch == '\''){
-//
-//        }
-//        }
-//    default:
-//        break;
-//    }
-//    return head;
-//}
+
+
+token* getTokenList() {//未完成：在状态转移过程中的token同时生成;有错误的处理方法
+    if (fp == nullptr) {
+        printf("文件打开失败！\n");
+        return nullptr;
+    }
+    //链表结点处理
+    token* head = (token*)malloc(sizeof(token)),*current=head,*next= (token*)malloc(sizeof(token));
+    init_node(head); init_node(next);
+    current->next = next; next->pre = current;
+    next->next = (token*)malloc(sizeof(token));
+
+    state state0 = START;
+    char ch = getNextChar();
+
+    //DFA字符接收器
+    char receiver[100]=" ";
+    int num = 0;
+
+    //未添加读新字符的循环
+    //添加出口:读完整个程序文件的出口->INRANGE_ERROR
+    //
+
+    switch (state0) {//处理逻辑：先看状态，再看输入
+    case START:
+        num = 0; receiver[0] = '\0';
+        switch (classify(ch)) {
+        case 1:
+            state0 = INID; receiver[num++] = ch; break;
+        case 2:
+            state0 = INNUM; receiver[num++] = ch; break;
+        case 3:
+            state0 = DONE; receiver[num++] = ch; break;
+        case 4:
+            receiver[num++] = ch;
+            ch = getNextChar();
+            if (ch == '=') {
+                state0 = INASSIGN; 
+                receiver[num++] = ch;
+                break;
+            }
+            else {
+                state0 = START;  error0 = INASSIGN_ERROR; break;//此处错误为赋值":="中':'后所跟并非'='
+            }
+        case 5:state0 = INCOMMENT;receiver[num++] = ch; break;//注释状态
+        case 6:
+            receiver[num++] = ch;
+            ch = getNextChar();
+            if (ch == '.') {
+                state0 = INRANGE; receiver[num++] = ch; break;//数组下标界限状态
+            }
+            else {
+                state0 = START; error0 = INRANGE_ERROR;  break;//此处错误为赋值":="中':'后所跟并非'='
+            }
+        case 7://无需录入
+            state0 = INCHAR; break;
+        default://输入无法识别
+            error0 = ERROR1; receiver[num++] = ch; break;
+        }
+        break;
+    case INID:
+        ch = getNextChar();
+        while (classify(ch) == 1 || classify(ch) == 2)
+        {
+            receiver[num++] = ch;
+            ch = getNextChar();
+        }
+        receiver[num] = '\0';
+        //token节点内容构建
+        strcpy(current->Sem,receiver);
+        current->Lex = ID;
+        current = next; next = next->next;//新current的指针信息是完整的
+        next->pre = current; next->next = (token*)malloc(sizeof(token));//完善next的指针信息
+        printf("识别标识符：%s\n", receiver);
+        //标识符识别，Token建立
+        ungetNextChar();
+        state0 = START;
+        break;
+    case INNUM:
+        ch = getNextChar();
+        while (classify(ch) == 2) {
+            receiver[num++] = ch;
+            ch = getNextChar();
+        }
+        receiver[num] = '\0';
+        //token节点内容构建
+        strcpy(current->Sem, receiver);
+        current->Lex = INTC;
+        current = next; next = next->next;//新current的指针信息是完整的
+        next->pre = current; next->next = (token*)malloc(sizeof(token));//完善next的指针信息
+        printf("识别数字：%s\n", receiver);
+        //标识符识别，Token建立
+        ungetNextChar();
+        state0 = START;
+        break;
+    case INASSIGN:
+        current->Lex = ASSIGN;
+        current = next; next = next->next;//新current的指针信息是完整的
+        next->pre = current; next->next = (token*)malloc(sizeof(token));//完善next的指针信息
+        printf("识别符号： :=\n");
+        state0 = START;
+        break;
+    case INCOMMENT://包含符号在内，不生成token
+        ch = getNextChar();
+        while (ch != '}')
+            ch = getNextChar();
+        printf("识别并已跳过注释");//不影响下一轮token生成
+        state0 = START;
+        break;
+    case INRANGE:
+        current->Lex = UNDERANGE;
+        current = next; next = next->next;//新current的指针信息是完整的
+        next->pre = current; next->next = (token*)malloc(sizeof(token));//完善next的指针信息
+        printf("识别符号：..（数组下标)\n");
+        state0 = START;
+        break;
+    case INCHAR:
+        ch = getNextChar();
+        if (classify(ch) == 1 || classify(ch) == 2) {
+            state0 = DONE;
+            //构建token
+            current->Lex = CHARC;
+            receiver[num++] = ch;
+            receiver[num] = '\0';
+            strcpy(current->Sem, receiver);
+            current = next; next = next->next;//新current的指针信息是完整的
+            next->pre = current; next->next = (token*)malloc(sizeof(token));//完善next的指针信息
+            printf("识别字符：..（数组下标)\n");
+        }
+        else {
+            state0 = START;
+            error0 = INCHAR_ERROR;
+        }
+        break;
+    case DONE:
+        if (classify(ch) == 1 || classify(ch) == 2) {//单字符识别
+            ch = getNextChar();
+            if (ch == '\'')
+                state0 = START;
+            else {
+                error0 = INCHAR_ERROR;
+                state0 = START;
+            }
+        }
+        else {//单分界符
+            current->Lex = classify1(ch);
+            current = next; next = next->next;//新current的指针信息是完整的
+            next->pre = current; next->next = (token*)malloc(sizeof(token));//完善next的指针信息
+            state0 = START;
+            printf("识别单分界符：%c（数组下标)\n",ch);
+        }
+        break;
+    default:
+        break;
+    }
+    return head;
+}
 int main() {
     //FILE* fp;
     //char ch;//取字符后放这里
@@ -168,9 +276,9 @@ int main() {
         printf("%c", test);
         test = getNextChar();
     }*/
-    char ab[100] = { '\0' };
-    char abc[100] = { 'a','b','c','d','\0' };
-    strcpy(ab, abc);
-    printf("%s", ab);
+    //char ab[100] = { '\0' };
+    //char abc[100] = { 'a','b','c','d','\0' };
+    //strcpy(ab, abc);
+    //printf("%s", ab);
 	return 0;
 }
