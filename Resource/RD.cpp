4693f2f229b8944a4ef6d6bc37a73efc6602bb72
PanDapao <1476822714@qq.com>
2022-04-10 18:36:22 +0800

    Update RD.cpp

--- Resource/RD.cpp
+++ Resource/RD.cpp
@@ -266,6 +266,7 @@ treenode* baseType()
 			initnode(newnode);
 			strcpy(newnode->str, "BaseType");
 			addChild(newnode, ReadmatchToken(INTEGER));
+			return newnode;
 		}
 	}
 	else if (nowtoken->Lex == CHAR)
@@ -276,99 +277,426 @@ treenode* baseType()
 		else
 		{
 			initnode(newnode);
-			strcpy(newnode->str, "TypeDecMore");
-			addChild(newnode, typeDecList());
+			strcpy(newnode->str, "BaseType");
+			addChild(newnode, ReadmatchToken(CHAR));
+			return newnode;
 		}
 	}
 	return newnode;
 }
 treenode* structureType()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == ARRAY)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "StructureType");
+			addChild(newnode, arrayType());
+			return newnode;
+		}
+	}
+	else if (nowtoken->Lex == RECORD)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "StructureType");
+			addChild(newnode, recType());
+			return newnode;
+		}
+	}
+	return newnode;
 }
 treenode* arrayType()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "ArrayType");
+		addChild(newnode, ReadmatchToken(ARRAY));
+		addChild(newnode, ReadmatchToken(LMIDPAREN));
+		addChild(newnode, ReadmatchToken(INTC));
+		addChild(newnode, ReadmatchToken(UNDERANGE));
+		addChild(newnode, ReadmatchToken(INTC));
+		addChild(newnode, ReadmatchToken(RMIDPAREN));
+		addChild(newnode, ReadmatchToken(OF));
+		addChild(newnode, baseType());
+	}
+	return newnode;
 }
 treenode* recType()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "RecType");
+		addChild(newnode,ReadmatchToken(RECORD));
+		addChild(newnode,fieldDecList());
+		addChild(newnode,ReadmatchToken(END));
+	}
+	return newnode;
 }
 treenode* fieldDecList()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == INTEGER || nowtoken->Lex == CHAR)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "FieldDecList");
+			addChild(newnode, baseType());
+			addChild(newnode, IDList());
+			addChild(newnode, ReadmatchToken(SEMI));
+			addChild(newnode, fieldDecMore());
+			return newnode;
+		}
+	}
+	else if (nowtoken->Lex == ARRAY)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "FieldDecList");
+			addChild(newnode, arrayType());
+			addChild(newnode, IDList());
+			addChild(newnode, ReadmatchToken(SEMI));
+			addChild(newnode, fieldDecMore());
+			return newnode;
+		}
+	}
+	return newnode;
 }
 treenode* IDList()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "IDList");
+		addChild(newnode, ReadmatchToken(ID));
+		addChild(newnode, IDMore());
+	}
+	return newnode;
 }
 treenode* fieldDecMore()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == INTEGER || nowtoken->Lex == CHAR || nowtoken->Lex == ARRAY)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "FieldDecMore");
+			addChild(newnode, fieldDecList());
+			return newnode;
+		}
+	}
+	return newnode;
 }
 treenode* IDMore()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == COMMA)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "IDMore");
+			addChild(newnode, ReadmatchToken(COMMA));
+			addChild(newnode, IDList());
+			return newnode;
+		}
+	}
+	return newnode;
 }
 treenode* varDec()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "VarDec");
+		addChild(newnode, ReadmatchToken(VAR));
+		addChild(newnode, varDecList());
+	}
+	return newnode;
 }
 treenode* varDecList()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "VarDecList");
+		addChild(newnode, typeDef());
+		addChild(newnode, varIDList());
+		addChild(newnode, ReadmatchToken(SEMI));
+		addChild(newnode, varDecMore());
+	}
+	return newnode;
 }
 treenode* varIDList()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "VarIDList");
+		addChild(newnode, ReadmatchToken(ID));
+		addChild(newnode, varIDMore());
+	}
+	return newnode;
 }
 treenode* varDecMore()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == INTEGER ||nowtoken->Lex == CHAR ||nowtoken->Lex == ARRAY ||nowtoken->Lex == RECORD ||nowtoken->Lex == ID)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "VarDecMore");
+			addChild(newnode, varDecList());
+			return newnode;
+		}
+	}
+	return newnode;
 }
 treenode* varIDMore()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == COMMA)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "VarIDMore");
+			addChild(newnode, ReadmatchToken(COMMA));
+			addChild(newnode, varIDList());
+			return newnode;
+		}
+	}
+	return newnode;
 }
 treenode* procDec()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "ProcDec");
+		addChild(newnode, ReadmatchToken(PROCEDURE));
+		addChild(newnode, ReadmatchToken(ID));
+		addChild(newnode, ReadmatchToken(LPAREN));
+		addChild(newnode, paramList());
+		addChild(newnode, ReadmatchToken(RPAREN));
+		addChild(newnode, ReadmatchToken(SEMI));
+		addChild(newnode, procDecPart());
+		addChild(newnode, procBody());
+		addChild(newnode, procDecpart());
+	}
+	return newnode;
 }
 treenode* paramList()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == INTEGER ||nowtoken->Lex == CHAR ||nowtoken->Lex == ARRAY ||nowtoken->Lex == RECORD ||nowtoken->Lex == VAR ||nowtoken->Lex == ID)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "ParamList");
+			addChild(newnode, paramDecList());
+			return newnode;
+		}
+	}
+	return newnode;
 }
 treenode* procDecPart()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "ProcDecPart");
+		addChild(newnode, declarePart());
+	}
+	return newnode;
 }
 treenode* procBody()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "ProcBody");
+		addChild(newnode, programBody());
+	}
+	return newnode;
 }
 treenode* paramDecList()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "ParamDecList");
+		addChild(newnode, param());
+		addChild(newnode, paramMore());
+	}
+	return newnode;
 }
 treenode* param()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == INTEGER ||nowtoken->Lex == CHAR ||nowtoken->Lex == ARRAY ||nowtoken->Lex == RECORD ||nowtoken->Lex == ID)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "Param");
+			addChild(newnode, typeDef());
+			addChild(newnode, formList());
+			return newnode;
+		}
+	}
+	else if (nowtoken->Lex == VAR)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "Param");
+			addChild(newnode, ReadmatchToken(VAR));
+			addChild(newnode, typeDef());
+			addChild(newnode, formList());
+			return newnode;
+		}
+	}
+	return newnode;
 }
 treenode* paramMore()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == SEMI)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "ParamMore");
+			addChild(newnode, ReadmatchToken(SEMI));
+			addChild(newnode, paramDecList());
+			return newnode;
+		}
+	}
+	return newnode;
 }
 treenode* formList()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "FormList");
+		addChild(newnode, ReadmatchToken(ID));
+		addChild(newnode, fidMore());
+	}
+	return newnode;
 }
 treenode* fidMore()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == COMMA)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "FidMore");
+			addChild(newnode, ReadmatchToken(COMMA));
+			addChild(newnode, formList());
+			return newnode;
+		}
+	}
+	return newnode;
 }
 treenode* stmList()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "StmList");
+		addChild(newnode, stm());
+		addChild(newnode, stmMore());
+	}
+	return newnode;
 }
 treenode* stm()
 {
