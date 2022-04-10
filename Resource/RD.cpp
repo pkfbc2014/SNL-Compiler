abf84177c2848503ea98e45b439b89ec52132377
PanDapao <1476822714@qq.com>
2022-04-10 16:38:53 +0800

    pkf

--- Resource/RD.cpp
+++ Resource/RD.cpp
@@ -9,6 +9,7 @@ void RD_analysis(token* tokenhead) // 递归下降分析法，接收token序列头
 {
 	nowtoken = tokenhead;
 
+
 }
 
 void initnode(treenode* temp) // 初始化节点
@@ -22,13 +23,18 @@ void initnode(treenode* temp) // 初始化节点
 
 treenode* ReadmatchToken(LexType tok)
 {
-
-	if (currentToken->wd.tok == tok)
+	if (tok == nowtoken->Lex) // 终结符匹配上了
 	{
-		TreeNode* t = new TreeNode(currentToken->wd.str);
-		t->tk = currentToken;
-		nowtoken = nowtoken->next; // token指针后移
-		return t;
+		treenode* newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			newnode->token = nowtoken;
+			nowtoken = nowtoken->next; // token指针后移
+			return newnode;
+		}
 	}
 	else
 	{
@@ -37,85 +43,244 @@ treenode* ReadmatchToken(LexType tok)
 	}
 }
 
+void addChild(treenode* root, treenode* child)
+{
+	root->child[root->childnum] = child;
+	root->childnum++;
+}
+
 void printerror() // 语法分析错误信息输出
 {
 
 }
 
 
-
 // 每个非终结符是一个函数，以下共58个函数（参考书上只给出了58个，虽然有67个非终结符）
 treenode* program()
 {
 	treenode* newnode = (treenode*)malloc(sizeof(treenode));
-	initnode(newnode);
-	strcpy(newnode->str, Non_symbol[0]);
-
-	newnode->child[newnode->childnum] = programHead();
-	newnode->childnum++;
-	newnode->child[newnode->childnum] = declarePart();
-	newnode->childnum++;
-	newnode->child[newnode->childnum] = programBody();
-	newnode->childnum++;
-
-	return newnode;
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "Program");
+		addChild(newnode, programHead());
+		addChild(newnode, declarePart());
+		addChild(newnode, programBody());
+		return newnode;
+	}
 }
 treenode* programHead()
 {
 	treenode* newnode = (treenode*)malloc(sizeof(treenode));
-	initnode(newnode);
-	strcpy(newnode->str, Non_symbol[1]);
-
-	newnode->child[newnode->childnum] = match()
-	newnode->childnum++;
-	newnode->child[newnode->childnum] = declarePart();
-	newnode->childnum++;
-
-	return newnode;
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "ProgramHead");
+		addChild(newnode, ReadmatchToken(PROGRAM));
+		addChild(newnode, ReadmatchToken(ID));
+		return newnode;
+	}
 }
 treenode* declarePart()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "DeclarePart");
+		addChild(newnode, typeDecPart());
+		addChild(newnode, varDecPart());
+		addChild(newnode, procDecpart());
+		return newnode;
+	}
 }
 treenode* programBody()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "DeclarePart");
+		addChild(newnode,ReadmatchToken(BEGIN));
+		addChild(newnode,stmList());
+		addChild(newnode,ReadmatchToken(END1));
+		return newnode;
+	}
 }
 treenode* typeDecPart()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == TYPE)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "TypeDecPart");
+			addChild(newnode, typeDec());
+		}
+	}
+	return newnode;
 }
 treenode* varDecPart()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == VAR)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "VarDecPart");
+			addChild(newnode, varDec());
+		}
+	}
+	return newnode;
 }
 treenode* procDecpart()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == PROCEDURE)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "ProcDecPart");
+			addChild(newnode, procDec());
+		}
+	}
+	return newnode;
 }
 treenode* typeDec()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "TypeDec");
+		addChild(newnode, ReadmatchToken(TYPE));
+		addChild(newnode, typeDecList());
+		return newnode;
+	}
 }
 treenode* typeDecList()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "TypeDecList");
+		addChild(newnode,ReadmatchToken(ID));
+		addChild(newnode,ReadmatchToken(EQ));
+		addChild(newnode,typeDef());
+		addChild(newnode,ReadmatchToken(SEMI));
+		addChild(newnode,typeDecMore());
+		return newnode;
+	}
 }
 treenode* typeID()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "TypeID");
+		addChild(newnode, ReadmatchToken(ID));
+		return newnode;
+	}
 }
 treenode* typeDef()
 {
-
+	treenode* newnode = (treenode*)malloc(sizeof(treenode));
+	if (newnode == NULL)
+		return NULL;
+	else
+	{
+		initnode(newnode);
+		strcpy(newnode->str, "TypeDef");
+		if ((nowtoken->Lex == INTEGER) || (nowtoken->Lex == CHAR))
+		{
+			addChild(newnode, baseType());
+			return newnode;
+		}
+		else if ((nowtoken->Lex == ARRAY) || (nowtoken->Lex == RECORD))
+		{
+			addChild(newnode, structureType());
+			return newnode;
+		}
+		else
+		{
+			addChild(newnode, ReadmatchToken(ID));
+			return newnode;
+		}
+	}
 }
 treenode* typeDecMore()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == ID)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "TypeDecMore");
+			addChild(newnode, typeDecList());
+		}
+	}
+	return newnode;
 }
 treenode* baseType()
 {
-
+	treenode* newnode = NULL;
+	if (nowtoken->Lex == INTEGER)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "BaseType");
+			addChild(newnode, ReadmatchToken(INTEGER));
+		}
+	}
+	else if (nowtoken->Lex == CHAR)
+	{
+		newnode = (treenode*)malloc(sizeof(treenode));
+		if (newnode == NULL)
+			return NULL;
+		else
+		{
+			initnode(newnode);
+			strcpy(newnode->str, "TypeDecMore");
+			addChild(newnode, typeDecList());
+		}
+	}
+	return newnode;
 }
 treenode* structureType()
 {
