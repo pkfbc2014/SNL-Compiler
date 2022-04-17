63633d4fc90ac7f7940dcbf9b77fa3352bda3d8c
1127960155 <1127960155@qq.com>
2022-04-17 11:37:48 +0800

    1

--- Resource/LL1_Head.h
+++ Resource/LL1_Head.h
@@ -8,8 +8,10 @@ treenode* createNode();
 void S_push(const char* str);//符号栈压栈
 void S_pop();
 void init_S_stack();//初始化符号栈
-void G_push();//语法树压栈
+void G_push(treenode* temp);//语法树压栈
 treenode* G_pop();
+void initnode_LL1(treenode* temp);
+treenode* createNode();
 
 void predict(int a);//产生式处理函数
 char* LexToStr(LexType temp);//终极符转为字符串
--- Resource/main.cpp
+++ Resource/main.cpp
@@ -18,17 +18,16 @@ int main()
 
     out_fitstfollow(); // 计算三个集合并输出到本地，LL1分析表存储在运行内存中（全局变量）
 
-    treenode* LL1TreeROOT = LL1_analysis(tokenlisthead);
-
-    // treenode* RDtreeROOT = RD_analysis(tokenlisthead); //语法分析 - 递归下降
+    treenode* LL1treeROOT = LL1_analysis(tokenlisthead);
+    //treenode* RDtreeROOT = RD_analysis(tokenlisthead); //语法分析 - 递归下降
 
     // 语义分析部分
 
-    // choosePrint(RDtreeROOT, 0); // 打印RD语法树
-    // choosePrint(LL1treeROOT, 1); // 打印LL1语法树
+    //choosePrint(RDtreeROOT, 0); // 打印RD语法树
+    choosePrint(LL1treeROOT, 1); // 打印LL1语法树
 
-    // freetree(RDtreeROOT); // 释放RD语法树
-    // freetree(LL1treeROOT); // 释放LL1语法树
+    //freetree(RDtreeROOT); // 释放RD语法树
+    freetree(LL1treeROOT); // 释放LL1语法树
     
 	return 0;
 } 
